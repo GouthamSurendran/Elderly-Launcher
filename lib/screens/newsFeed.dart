eafa1dd3651e76e09584fe256b5b3e692afdaf0c
GouthamSurendran <gouthu.g3@gmail.com>
2021-04-19 18:49:36 +0530

    news scree

--- lib/screens/newsFeed.dart
+++ lib/screens/newsFeed.dart
@@ -1,31 +1,34 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:senior_launcher/widgets.dart';
 
-const url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=05d88a72b6f548c2aa621ab93961ed9b";
+const url =
+    "https://newsapi.org/v2/top-headlines?country=in&apiKey=05d88a72b6f548c2aa621ab93961ed9b";
 const defaultImage = "https://www.ledr.com/colours/black.jpg";
 
-class NewsFeed extends StatefulWidget  {
+class NewsFeed extends StatefulWidget {
   @override
   _NewsFeedState createState() => _NewsFeedState();
 }
 
-class _NewsFeedState extends State<NewsFeed> with AutomaticKeepAliveClientMixin <NewsFeed> {
-  @override
-  bool get wantKeepAlive => true;
+class _NewsFeedState extends State<NewsFeed> {
+  //bool get wantKeepAlive => true;
 
   List<Widget> newsItems = [];
   int numNews;
 
   Future getNews() async {
     newsItems.clear();
+    int timeout = 5;
     var response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
       int numNews = data["totalResults"];
 
-      for (int i=0;i < numNews; i++){
+      for (int i = 0; i < numNews; i++) {
         String auth = "";
         String ur = "";
         if (data["articles"][i]["author"] == null)
@@ -36,7 +39,8 @@ class _NewsFeedState extends State<NewsFeed> with AutomaticKeepAliveClientMixin 
           ur = defaultImage;
         else
           ur = data["articles"][i]["urlToImage"];
-        newsItems.add(NewsItem(auth, data["articles"][i]["title"], ur,data["articles"][i]["url"],data["articles"][i]["content"]));
+        newsItems.add(NewsItem(auth, data["articles"][i]["title"], ur,
+            data["articles"][i]["url"], data["articles"][i]["content"]));
       }
       return newsItems;
     }
@@ -58,28 +62,32 @@ class _NewsFeedState extends State<NewsFeed> with AutomaticKeepAliveClientMixin 
       body: SafeArea(
         child: RefreshIndicator(
           child: FutureBuilder(
-            builder: (context,snap){
-                if (snap.connectionState == ConnectionState.none && snap.hasData == null){
-                  return Container();
+            builder: (context, snap) {
+              if (snap.connectionState == ConnectionState.none &&
+                  snap.hasData == null) {
+                return Container();
               }
-                return ListView.builder(
+              return ListView.builder(
                   physics: BouncingScrollPhysics(),
-                    itemCount: newsItems.length,
-                    itemBuilder: (context,index){
-                      return newsItems[index];
-                });
+                  itemCount: newsItems.length,
+                  itemBuilder: (context, index) {
+                    return newsItems[index];
+                  });
             },
             future: getNews(),
           ),
-          onRefresh: () {
-            return getNews();
+          onRefresh: () async {
+            try {
+              final result = await InternetAddress.lookup('google.com');
+              if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+                return getNews();
+              }
+            } on SocketException catch (e) {
+              //print(e);
+            }
           },
         ),
       ),
     );
   }
 }
-
-
-
-
